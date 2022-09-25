ba8ab1ebda6bf667703901f6285d0d7f16585eec
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-24 22:16:25 -0700

    Add files via upload

---
+++ my-blog/src/pages/article-content.js
@@ -0,0 +1,19 @@
+const articles = [
+    {
+        name: 'one',
+        title: 'first artcile',
+        content: [
+            `The first article in document`,
+            `see next`
+        ]
+    },{
+        name: 'two',
+        title: 'second article',
+        content: [
+            `second article is better because article is not mispelled`,
+            `see previous`
+        ]
+    }
+]
+
+export default articles;
