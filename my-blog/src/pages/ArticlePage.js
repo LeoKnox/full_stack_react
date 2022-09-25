9fc6974b5da1cc579fa90538fa4e6cc5d36d4937
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-24 22:19:44 -0700

    Add files via upload

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const ArticlePage = () => {
     const params = useParams();
+    const { articleId } = params;
 
     return (
         <h1>Article Page</h1>
