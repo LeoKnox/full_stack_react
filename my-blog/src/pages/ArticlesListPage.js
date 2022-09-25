67c050eaee9102c273ec6025653a91346f7a8666
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-24 23:34:44 -0700

    Add files via upload

--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -7,7 +7,7 @@ const ArticlesListPage = () => {
         <h1>Articles List Page</h1>
         {articles.map(article => (
             <>
-            <Link to={`/articles/${article.name}`}>
+            <Link key={article.name} to={`/articles/${article.name}`}>
                 <h3>{article.title}</h3>
             </Link>
             <p>{article.content[0].substring(0, 150)}...</p>
