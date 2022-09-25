06ac7c969ab397c477719e2a8b888e3198a4eddc
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-24 22:26:33 -0700

    Add files via upload

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,11 +1,18 @@
 // localhost:3000/articles/one
 import { useParams } from 'react-router-dom';
+import articles from './article-content';
 
 const ArticlePage = () => {
     const { articleId }= useParams();
+    const article = articles.find(article => article.name === articleId);
 
     return (
-        <h1>Article Page for id: {articleId}</h1>
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map(paragraph => (
+                <p>{paragraph}</p>
+            ))}
+        </>
     )
 }
 
