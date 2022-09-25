0ca81ce9b9b74cc99a1c9ed51b52dbea6a0c6b7e
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-24 23:27:22 -0700

    Add files via upload

--- my-blog/src/pages/ArticlesListPage.js
+++ my-blog/src/pages/ArticlesListPage.js
@@ -1,6 +1,16 @@
+import articles from './article-content.js';
+
 const ArticlesListPage = () => {
     return (
+        <>
         <h1>Articles List Page</h1>
+        {articles.map(article => (
+            <div>
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0, 150)}</p>
+            </div>
+        ))}
+        </>
     )
 }
 
